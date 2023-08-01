b4efd8f1d5fc5ae479f1d7c0af843d70611c7f63
Vincenzo Villani <v.villani@iconsulting.biz>
2023-08-01 10:28:25 +0200

    added prime multiselect

---
+++ src/app/global-di-components/components/multiselect.js
@@ -0,0 +1,16 @@
+import { ComponentsRegistry } from "cle.js/lib";
+import { RC } from "../../generic-rc-wrapper";
+import { Prime } from "../../primereact.all";
+
+ComponentsRegistry.define({ Multiselect: ({selected, onSelectionChange, options, usedCleProps, useReactArgs, extraDef, ...props}={})=>{
+
+    return RC(Prime.MultiSelect, { 
+        usedCleProps,
+        f: { ...props, value: selected, options, onChange: onSelectionChange}, 
+        useReactArgs: useReactArgs ?? [undefined, undefined, extraDef] // extra def for the le wrapper!
+    }
+    
+    )['nested-react'] 
+
+}}, null)
+
--- src/app/primereact.all.jsx
+++ src/app/primereact.all.jsx
@@ -1,10 +1,12 @@
 // import used primereact component under "Prime" and "P" name
 import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext"
+import { MultiSelect } from 'primereact/multiselect';
 
 export const Prime = {
     Button,
-    InputText
+    InputText,
+    MultiSelect
 }
 
 export const P = Prime
